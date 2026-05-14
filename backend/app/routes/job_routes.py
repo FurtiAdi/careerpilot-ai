f692a0665534992c1bbf7058c67b7dd9d3c80c36
FurtiAdi <furtiadi@gmail.com>
2026-05-15 01:18:10 +0200

    Add PDF resume text extraction support

--- backend/app/routes/job_routes.py
+++ backend/app/routes/job_routes.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
+import tempfile
 
+from app.services.resume_service import extract_text_from_pdf
 from app.models.job_models import JobRequest
 from app.services.ai_service import generate_ai_analysis
 
@@ -33,4 +35,27 @@ def analyze_job(job: JobRequest):
         "candidate_skills": candidate_skills,
         "match_analysis": score_results,
         "ai_analysis": ai_analysis
+    }
+
+@router.post("/upload-resume")
+async def upload_resume(
+    file: UploadFile = File(...)
+):
+
+    with tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=".pdf"
+    ) as temp_file:
+
+        temp_file.write(await file.read())
+
+        temp_file_path = temp_file.name
+
+    extracted_text = extract_text_from_pdf(
+        temp_file_path
+    )
+
+    return {
+        "filename": file.filename,
+        "extracted_text": extracted_text
     }
