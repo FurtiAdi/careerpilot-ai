505d543bbfb11dac6e6d021ee4eab651b3ccad91
fortuna-ot <fortuna@optimaltrappstadning.se>
2026-08-17 10:51:44 +0200

    refactor: separate profile routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.job_routes import router as job_router
 from app.routes.analysis_routes import router as analysis_router
 from app.routes.auth_routes import router as auth_router
+from app.routes.profile_routes import router as profile_router
 from app.models.user_model import User
 from fastapi.staticfiles import StaticFiles
 
@@ -44,3 +45,4 @@ def home():
 app.include_router(job_router)
 app.include_router(analysis_router)
 app.include_router(auth_router)
+app.include_router(profile_router)
